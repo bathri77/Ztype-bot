21186b65cdd35021d24ab8dae07d91a6e9f8d498
Bathrinarayana <78746273+bathri77@users.noreply.github.com>
2023-07-01 23:03:02 +0530

    Add files via upload

---
+++ Ztype Bot.py
@@ -0,0 +1,7 @@
+import pyautogui as pag
+import time
+
+time.sleep(5)
+
+while True:
+		pag.typewrite(['e','t','a','i','n','o','s','h','r','d','l','u','c','m','f','w','y','g','p','b','v','k','q','j','x','z','e','t','a','i','n','o','s','h','r','d','l','u','c','m','f','w','y','g','p','b','v','k','q','j','x','z','e','t','a','i','n','o','s','h','r','d','l','u','c','m','f','w','y','g','p','b','v','k','q','j','x','z','e','t','a','i','n','o','s','h','r','d','l','u','c','m','f','w','y','g','p','b','v','k','q','j','x','z','e','t','a','i','n','o','s','h','r','d','l','u','c','m','f','w','y','g','p','b','v','k','q','j','x','z','e','t','a','i','n','o','s','h','r','d','l','u','c','m','f','w','y','g','p','b','v','k','q','j','x','z','e','t','a','i','n','o','s','h','r','d','l','u','c','m','f','w','y','g','p','b','v','k','q','j','x','z'],interval=0.0001)
